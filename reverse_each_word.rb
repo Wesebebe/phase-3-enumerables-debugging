c17421c232e0c045d693e39c42cdb5e1b034caaf
+ <+>
2023-05-19 13:18:10 +0300

    Complete assignment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,41 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = []
+
+    words.each do |word|
+        reversed_words << word.reverse
+    end
+    reversed_words.join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
+
+# method 2
+
+def reverse_each_word(sentence)
+  words = sentence.split
+
+  reversed_words = words.map do |word|
+    word.reverse
+  end
+
+  reversed_words.join(" ")
+end
+
+# using proc shorthand
+
+def reverse_each_word(sentence)
+  words = sentence.split
+
+  reversed_words = words.map(&:reverse)
+
+  reversed_words.join(" ")
+end
+
+# using chaining
+
+def reverse_each_word(sentence)
+  sentence.split.map(&:reverse).join(" ")
+end
